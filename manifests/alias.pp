6b13ea5cce6c748fbbf11100b1e289d1ea55ddef
Zane Ma <zane@betterment.com>
2015-03-16 10:22:31 -0400

    move key length security plugin to versions

--- manifests/alias.pp
+++ manifests/alias.pp
@@ -29,47 +29,9 @@ define java::alias (
 
   $sys_version = java_download_version_to_sys_version($to)
   $jdk_dir = "/Library/Java/JavaVirtualMachines/jdk${version}.jdk"
-  $jenv_versions = "${java::jenv::prefix}/versions"
-  $sec_dir = "${jdk_dir}/Contents/Home/jre/lib/security"
 
   file { [$jdk_dir]:
     ensure  => $dir_ensure,
     force   => true
   } 
-
-  # Allow 'large' keys locally.
-  # http://www.ngs.ac.uk/tools/jcepolicyfiles
-  file { $sec_dir:
-    ensure  => 'directory',
-    owner   => 'root',
-    group   => 'wheel',
-    mode    => '0775',
-    require => File[$jdk_dir]
-  }
-
-  file { "${sec_dir}/local_policy.jar":
-    source  => 'puppet:///modules/java/local_policy.jar',
-    owner   => 'root',
-    group   => 'wheel',
-    mode    => '0664',
-    require => File[$sec_dir]
-  }
-
-  file { "${sec_dir}/US_export_policy.jar":
-    source  => 'puppet:///modules/java/US_export_policy.jar',
-    owner   => 'root',
-    group   => 'wheel',
-    mode    => '0664',
-    require => File[$sec_dir]
-  }
-
-  # add the version to jenv - use jenv's add in the future?
-  file { "${jenv_versions}/${version}":
-    ensure  => $link_ensure,
-    target  => "${jdk_dir}/Contents/Home",
-    force   => true,
-    require => File["${jenv_versions}"]
-  }
 }
-
-# }
--- manifests/version.pp
+++ manifests/version.pp
@@ -28,8 +28,13 @@ define java::version(
 
     include boxen::config
 
+    $sys_version = java_download_version_to_sys_version($version)
     $jre_url = "${base_download_url}/jre-${version}-macosx-x64.dmg"
     $jdk_url = "${base_download_url}/jdk-${version}-macosx-x64.dmg"
+    $jenv_versions = "${java::jenv::prefix}/versions"
+    $jdk_dir = "/Library/Java/JavaVirtualMachines/jdk${sys_version}.jdk"
+    $sec_dir = "${jdk_dir}/Contents/Home/jre/lib/security"
+
     # if ((versioncmp($::macosx_productversion_major, '10.10') >= 0) and
     #   versioncmp($update_version, '71') < 0)
     # {
@@ -47,6 +52,38 @@ define java::version(
         alias    => "java-${version}",
         provider => pkgdmg,
         source   => $jdk_url ;
+    }
+
+    file { $sec_dir:
+      ensure  => 'directory',
+      owner   => 'root',
+      group   => 'wheel',
+      mode    => '0775',
+      require => Package["java-${version}"]
+    }
+
+    file { "${sec_dir}/local_policy.jar":
+      source  => 'puppet:///modules/java/local_policy.jar',
+      owner   => 'root',
+      group   => 'wheel',
+      mode    => '0664',
+      require => File[$sec_dir]
+    }
+
+    file { "${sec_dir}/US_export_policy.jar":
+      source  => 'puppet:///modules/java/US_export_policy.jar',
+      owner   => 'root',
+      group   => 'wheel',
+      mode    => '0664',
+      require => File[$sec_dir]
+    }
+
+    # add the version to jenv - use jenv's add in the future?
+    file { "${jenv_versions}/${version}":
+      ensure  => $link_ensure,
+      target  => "${jdk_dir}/Contents/Home",
+      force   => true,
+      require => File["${jenv_versions}"]
     }
   }
 }
